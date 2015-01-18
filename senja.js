cdc6707cbeba6bb8e58878e42cc179be461757be
Tuomas Starck <dev@starck.fi>
2015-01-19 01:54:41 +0200

    Poster and first version of the CV

--- senja.js
+++ senja.js
@@ -1,26 +1,26 @@
 
-var matrix = [
-  { width: 1280, height: 720,  img: '720.jpg' },
-  { width: 1920, height: 1080, img: '1080.jpg' },
-  { width: 2560, height: 1440, img: '1440.jpg' },
-  { width: 3840, height: 2160, img: '4k.jpg' },
-]
+var background = [
+  { width:  640, height:  360, img: '/imgs/mini.jpg' },
+  { width: 1280, height:  720, img: '/imgs/1280.jpg' },
+  { width: 1920, height: 1080, img: '/imgs/1920.jpg' },
+  { width: 2560, height: 1440, img: '/imgs/2560.jpg' },
+  { width: 3840, height: 2160, img: '/imgs/3840.jpg' },
+];
 
 var choose = function(x, y) {
-  var img = 'mini.jpg';
-  matrix.forEach(function(opt) {
-    if (x >= opt.width || y >= opt.height) {
-      console.log('on ainakin ' + opt.img)
-      img = opt.img
+  var img = background[0].img;
+  background.forEach(function(bg) {
+    if (x >= bg.width || y >= bg.height) {
+      img = bg.img;
     }
-  })
-  return img
-}
+  });
+  return img;
+};
 
 var change = function() {
-  var img = choose(window.screen.availWidth, window.screen.availHeight)
-  var html = document.getElementsByTagName('html')[0]
-  html.style.backgroundImage = 'url(' + img + ')'
-}
+  var img = choose(window.screen.availWidth, window.screen.availHeight);
+  var html = document.getElementsByTagName('html')[0];
+  html.style.backgroundImage = 'url(' + img + ')';
+};
 
-window.onload = setTimeout(change, 999)
+window.onload = setTimeout(change, 1);
